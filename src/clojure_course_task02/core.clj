143636ce76fc896d4e12c93209a03b43e868d2bb
Bakuta Andrey <dra1n86@gmail.com>
2013-03-28 08:22:00 +0200

    adsfasdasd

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -2,12 +2,32 @@
   (:import java.io.File)
   (:gen-class))
 
+(def results (agent #{}))
+
+(defn zip [& coll]
+  (apply map vector coll))
+
+(defn check-name [name pat]
+  (if-let [match (re-matches (re-pattern pat) name)]
+    (send results conj match)))
+
+(defn make-search! [file-name d]
+  (if (.isDirectory d)
+    (let [files (.listFiles d)
+          agents (doall (map #(agent %) files))
+          jobs (zip agents files)]
+      (doseq [job jobs] (send (first jobs) (last jobs))))))
+
+(comment(defn make-search! [file-name d]
+  (pmap (fn [f]
+    (if (.isDirectory f)
+      (make-search! file-name f)
+      (check-name (.getName f) file-name)))
+    (.listFiles d))))
 
 (defn find-files [file-name path]
-  (doseq [f (.listFiles (File. path))]
-    (if (.isDirectory f)
-      (find-files file-name f)
-      (println (re-matches (re-pattern file-name) (.getName f))))))
+  (make-search! file-name (File. path)))
+  
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -18,9 +38,5 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
-
-(def d (File. "./"))
-
-(.listFiles d)
-
+      (dorun (map println (find-files file-name path)))))
+      (shutdown-agents))
